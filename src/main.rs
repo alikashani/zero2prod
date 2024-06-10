473f6c19931362d962ecc6f96316373059cce0a1
Ali Kashani <1kashaniali@gmail.com>
2024-06-10 13:23:25 -0700

    Format changes; update actions/checkout v3 to v4

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read configuration
-    let configuration = get_configuration()
-        .expect("Failed to read configuration.");
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener)?.await
